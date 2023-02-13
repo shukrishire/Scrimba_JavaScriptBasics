db0cb6bb8bdeaaf7ae381e3b4678624c6c1dea19
shukrishire <shukrishire28@gmail.com>
2023-02-13 14:20:21 +1300

    secondversion

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const app = document.querySelector<HTMLDivElement>('#app')!
+//console.log(document.getElementById("count-el").innerText )
+// add the set timer here to see 
+//document.getElementById("count-el").innerText = 2;
+//console.log(document.getElementById("count-el").innerText )
+// let count = 0
+// console.log(count)
 
+//1. Create a variable, myAge, and set its value to yoru age 
+//let myAge = 36
+//2. Log the myAge variable to the console
+//console.log(myAge)
+let firstBatch = 5;
+let secondBatch = 7;  
 
-document.getElementById("count-el").innerText = 5 
-
-
-export { }
+let count = firstBatch + secondBatch;
+console.log(count)
