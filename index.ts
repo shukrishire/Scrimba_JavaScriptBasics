277c88a7f53d6fd23c7c216d8e1403be316fcf9f
shukrishire <shukrishire28@gmail.com>
2023-02-13 15:12:37 +1300

    changed the JS to TS

--- index.ts
+++ index.ts
@@ -9,8 +9,29 @@
 //let myAge = 36
 //2. Log the myAge variable to the console
 //console.log(myAge)
-let firstBatch = 5;
-let secondBatch = 7;  
+// let firstBatch = 5;
+// let secondBatch = 10;  
 
-let count = firstBatch + secondBatch;
+// let count = firstBatch + secondBatch;
+// console.log(count)
+
+//1. 1 human years ~ dog years challenge 
+// let myAge = 35;
+// let humanDogRatio = 7;
+// let myDogAge = myAge * humanDogRatio; 
+// console.log(myDogAge)
+
+
+let count = 50
 console.log(count)
+
+let bonusPoint = count + 50 
+console.log(bonusPoint)
+
+bonusPoint = bonusPoint  - 75 
+console.log(bonusPoint)
+
+
+bonusPoint = bonusPoint  + 45
+console.log(bonusPoint)
+
