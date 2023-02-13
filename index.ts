7c8eee5505132b9f38422bb5a0907984ae27903e
shukrishire <shukrishire28@gmail.com>
2023-02-13 14:53:03 +1300

    old versionwith prev stuff

---
+++ index.ts
@@ -0,0 +1,16 @@
+//console.log(document.getElementById("count-el").innerText )
+// add the set timer here to see 
+//document.getElementById("count-el").innerText = 2;
+//console.log(document.getElementById("count-el").innerText )
+// let count = 0
+// console.log(count)
+
+//1. Create a variable, myAge, and set its value to yoru age 
+//let myAge = 36
+//2. Log the myAge variable to the console
+//console.log(myAge)
+let firstBatch = 5;
+let secondBatch = 7;  
+
+let count = firstBatch + secondBatch;
+console.log(count)
